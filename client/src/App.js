5322bf13e7ad94663892e92b30f0214acb011b82
Simone Roy <simonecarolynr@gmail.com>
2020-01-20 18:35:27 -0700

    react-component

--- client/src/App.js
+++ client/src/App.js
@@ -14,8 +14,8 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <Route exact path='/' component={MovieList} />
-      <Route path='/movies/:id' component={Movie}/>
+      <Route exact path='/' render= {(props) => {return <MovieList {...props}/>}} />
+      <Route path='/movies/:id' render={(props) =>{return <Movie {...props}/>}}/>
     </div>
   );
 };
